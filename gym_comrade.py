8d25cb336685818f4f80636de755b826f1f64398
BeefyBison <94725754+BeefyBison@users.noreply.github.com>
2022-03-17 09:35:12 -0400

    Feat: Restructured main application to use Kivy
    Removed use of tkinter and added a kv file and rebuilt the gym_comrade program to use kivy.
    This allows the use of the program on iphone and android as well as a rescalable window on the computer.
    
    Main issues currently with this new version are:
    - Not generating more than just the cardio list
    - Clearing all the checkboxes after clicking 'Generate'
    - Unchecking a box doesn't undo the append to the final_workout list
    - The list is cleared even if boxes remained checked
    - The only checkbox that seems to be picked up by on_checkbox_Active is the cardio checkbox
    
    Mainly things related to the checkboxes.

--- gym_comrade.py
+++ gym_comrade.py
@@ -6,116 +6,185 @@ Written by Beefybison
 
 """
 
-import tkinter as tk
-from tkinter import TOP, X, GROOVE
 import random
-from checkbar import Checkbar
+from select import select
+from kivy.uix.widget import Widget
+from kivy.app import App
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
 
-if __name__ == "__main__":
+MUSCLES = ("Biceps", "Triceps", "Chest", "Back", "Legs", "Shoulders", "Cardio")
 
-    def write_workout():
-        """This function is what posts the workout when the button is pressed"""
-
-        workout_quantity_var = random.choice(range(2, 4))
-        mgroup = list(lng.state())
-
-        if mgroup[0] == 1:
-            for _i in range(1):
-                T.insert(tk.END, random.choice(range(2, 6)))
-                T.insert(tk.END, " sets:\n")
-                T.insert(tk.END, random.sample(biceps, workout_quantity_var))
-                T.insert(tk.END, "\n")
-        else:
-            pass
-
-        if mgroup[1] == 1:
-            for _i in range(1):
-                T.insert(tk.END, random.choice(range(2, 6)))
-                T.insert(tk.END, " sets:\n")
-                T.insert(tk.END, random.sample(triceps, workout_quantity_var))
-                T.insert(tk.END, "\n")
-        else:
-            pass
-
-        if mgroup[2] == 1:
-            for _i in range(1):
-                T.insert(tk.END, random.choice(range(2, 6)))
-                T.insert(tk.END, " sets:\n")
-                T.insert(tk.END, random.sample(chest, workout_quantity_var))
-                T.insert(tk.END, "\n")
-        else:
-            pass
-
-        if mgroup[3] == 1:
-            for _i in range(1):
-                T.insert(tk.END, random.choice(range(2, 6)))
-                T.insert(tk.END, " sets:\n")
-                T.insert(tk.END, random.sample(back, workout_quantity_var))
-                T.insert(tk.END, "\n")
-        else:
-            pass
-
-        if mgroup[4] == 1:
-            for _i in range(1):
-                T.insert(tk.END, random.choice(range(2, 6)))
-                T.insert(tk.END, " sets:\n")
-                T.insert(tk.END, random.sample(legs, workout_quantity_var))
-                T.insert(tk.END, "\n")
-        else:
-            pass
-
-    root = tk.Tk()
-    frame = tk.Frame(root)
-    frame.pack()
-
-    T = tk.Text(root, height=30, width=100)
-    T.pack(side=tk.LEFT)
-    scroll = tk.Scrollbar(root, command=T.yview)
-    T.configure(yscrollcommand=scroll.set)
-    scroll.pack(side=tk.RIGHT, fill=tk.Y)
-
-    exit_button = tk.Button(frame, text="QUIT", fg="red", command=quit)
-    exit_button.pack(side=tk.LEFT)
-
-    begin_button = tk.Button(frame, text="Generate", fg="green", command=write_workout)
-    begin_button.pack(side=tk.LEFT)
-
-    muscles = ["Biceps", "Triceps", "Chest", "Back", "Legs"]
-    lng = Checkbar(root, muscles)
-    lng.pack(side=TOP, fill=X)
-    lng.config(relief=GROOVE, bd=2)
-
-    triceps = [
-        "skullcrushers",
-        "tricep kickback",
-        "palms up triceps band kickback",
-        "standing single arm triceps band extensions",
-        "standing triceps band pushdowns",
-        "kneeling triceps band pushdowns",
-        "overhead triceps band extensions",
+BICEPS = [
+    "barbell curls",
+    "dumbbell curls",
+    "hammber curls",
+    "rope curls",
+    "preacher curls",
     ]
 
-    biceps = [
-        "barbell curls",
-        "dumbbell curls",
-        "hammber curls",
-        "rope curls",
-        "preacher curls",
+TRICEPS = [
+    "skullcrushers",
+    "tricep kickback",
+    "palms up triceps band kickback",
+    "standing single arm triceps band extensions",
+    "standing triceps band pushdowns",
+    "kneeling triceps band pushdowns",
+    "overhead triceps band extensions",
     ]
 
-    chest = [
-        "bench press",
-        "arnold press",
-        "resistance band chest press",
-        "banded crossover push up",
-        "resistance band low to high crossovers",
+CHEST = [
+    "bench press",
+    "arnold press",
+    "dumbbell flyes",
+    "banded crossover push up",
+    "resistance band low to high crossovers",
     ]
 
-    back = ["rows", "lat pulldown", "reverse flyes", "kettlebell swings"]
-
-    legs = [
-        "squats",
-        "resistance band deadlift",
-        "resistance band stiff-legged deadlift",
-        "resistance band lunge to squat",
+BACK = [
+    "upright rows",
+    "rows",
+    "lat pulldown",
+    "reverse flyes",
+    "kettlebell swings"
     ]
+
+LEGS = [
+    "squats",
+    "resistance band deadlift",
+    "resistance band stiff-legged deadlift",
+    "resistance band lunge to squat",
+    ]
+
+SHOULDERS = [
+    "lateral raises",
+    "military press",
+    "incline benchpress",
+    ]
+
+CARDIO = [
+    "star jumps",
+    "jumping lunges",
+    "high kness",
+    ]
+
+final_workout = []
+workout_quantity_var = random.choice(range(2, 4)) # change theses numbers to add more or less workouts
+
+class gym_comrade(App):
+    """Configures window and adds checkboxes, widgets, and labels."""
+
+    def build(self):
+        self.window = GridLayout()
+        self.window.cols = 1
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+
+        self.greeting = Label(
+            text="What workout would you like today?",
+            font_size = 25,
+            color="#00FFCE"
+            )
+        self.window.add_widget(self.greeting)
+    
+        # Add checkbox, widget and labels
+        self.window.add_widget(Label(text =''))
+        self.active = CheckBox(active = False)
+        self.window.add_widget(self.active)
+
+        self.lbl_active = Label(text ='Biceps')
+        self.window.add_widget(self.lbl_active)
+
+        self.active.bind(active = self.on_checkbox_Active)
+    
+        self.window.add_widget(Label(text =''))
+        self.active = CheckBox(active = False)
+        self.window.add_widget(self.active)
+
+        self.lbl_active = Label(text ='Triceps')
+        self.window.add_widget(self.lbl_active)
+
+        self.active.bind(active = self.on_checkbox_Active)
+    
+        self.window.add_widget(Label(text =''))
+        self.active = CheckBox(active = False)
+        self.window.add_widget(self.active)
+
+        self.lbl_active = Label(text ='Chest')
+        self.window.add_widget(self.lbl_active)
+
+        self.active.bind(active = self.on_checkbox_Active)
+
+        self.window.add_widget(Label(text =''))
+        self.active = CheckBox(active = False)
+        self.window.add_widget(self.active)
+
+        self.lbl_active = Label(text ='Back')
+        self.window.add_widget(self.lbl_active)
+
+        self.active.bind(active = self.on_checkbox_Active)
+
+        self.window.add_widget(Label(text =''))
+        self.active = CheckBox(active = False)
+        self.window.add_widget(self.active)
+
+        self.lbl_active = Label(text ='Legs')
+        self.window.add_widget(self.lbl_active)
+
+        self.active.bind(active = self.on_checkbox_Active)
+
+        self.window.add_widget(Label(text =''))
+        self.active = CheckBox(active = False)
+        self.window.add_widget(self.active)
+
+        self.lbl_active = Label(text ='Shoulders')
+        self.window.add_widget(self.lbl_active)
+
+        self.active.bind(active = self.on_checkbox_Active)
+
+        self.window.add_widget(Label(text =''))
+        self.active = CheckBox(active = False)
+        self.window.add_widget(self.active)
+
+        self.lbl_active = Label(text ='Cardio')
+        self.window.add_widget(self.lbl_active)
+
+        self.active.bind(active = self.on_checkbox_Active)
+
+        self.button = Button(
+            text="GENERATE",
+            bold = True,
+            background_color = '#00FCE'
+            )
+        self.button.bind(on_press=self.on_press)
+        self.window.add_widget(self.button)
+
+        return self.window
+
+    def on_checkbox_Active(checkbox, value, isActive):
+        """Checks if the checkbox is active & primes the workout list."""
+        if isActive == True:
+            print(checkbox.lbl_active.text)
+            if checkbox.lbl_active.text == 'Cardio':
+                write_workout(CARDIO)
+            return True
+        else:
+            return False
+
+    def on_press(self, instance):
+        """Updates the screen with the final workout."""
+        self.greeting.font_size = 15
+        self.greeting.text = str(final_workout)
+        final_workout.clear()
+
+def write_workout(muscle):
+    """Generates the workout."""
+    select_workout = str(random.sample(muscle, workout_quantity_var))
+    final_workout.append(select_workout)
+
+if __name__ == '__main__':
+    gym_comrade().run()
